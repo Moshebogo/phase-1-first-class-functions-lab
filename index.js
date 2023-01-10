c6931cc2eb064fd6707d7cdc1d4065531b1a5ee2
Eli Moshe <moshebogo@gmail.com>
2023-01-10 03:10:05 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const  returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2, 4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (integer) => {
+  const fareDoubler = (fare) => {
+    return fare * integer;
+ }
+ return fareDoubler;
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+fareTripler = (fare) => fare * 3;
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
